467e40f692d743dc7b2ed8786c4dafcc65303c85
choi <ww1016y@gmail.com>
2019-08-14 05:18:47 +0000

    h

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/workspace/project4/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/workspace/project4/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/workspace/project4/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/workspace/project4/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/workspace/project4/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/jeongae13choi/project3/cakin_ws/src:/home/workspace/project4/cakin_ws/src:/home/jeongae13choi/project4/catkin_ws/src:/home/workspace/project4/catkin_ws/src:$ROS_PACKAGE_PATH"
