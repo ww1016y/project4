5c4cb9353c02f042e0f88890936e9dba205a571e
ww1016y <ww1016y@gmail.com>
2019-08-14 11:13:05 +0900

    second

--- cakin_ws/build/my_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/project4/cakin_ws/src/my_robot
-# Build directory: /home/workspace/project4/cakin_ws/build/my_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
